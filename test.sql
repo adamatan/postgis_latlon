4257b5c438ff28b205f18985ca6384801dadec12
Adam Matan <adam@matan.name>
2016-03-08 09:24:08 +0200

    Add tests

--- test.sql
+++ test.sql
@@ -8,11 +8,11 @@ SELECT latlon(geom),
        latlon_is_valid(geom)
 FROM (SELECT st_geomFromText('POINT(2.294609 48.85835)', 4326) as geom) sq;
 
-SELECT 'Validity - valid polygon.' AS TEST;
+SELECT 'latlon_is_valid - valid polygon.' AS TEST;
 SELECT latlon_is_valid(geom) AS should_be_true
 FROM (SELECT st_geomFromText('POLYGON((2.291 48.861,2.298 48.861,2.298 48.855,2.291 48.855,2.291 48.861))', 4326) as geom) sq;
 
-SELECT 'Validity - invalid polygons.' AS TEST;
+SELECT 'latlon_is_valid - invalid polygons.' AS TEST;
 SELECT latlon_is_valid(geom) AS should_be_false
 FROM (SELECT st_geomFromText('POLYGON((182.291 48.861,2.298 48.861,2.298 48.855,2.291 48.855,182.291 48.861))', 4326) as geom) sq;
 
@@ -22,11 +22,11 @@ FROM (SELECT st_geomFromText('POLYGON((2.291 148.861,2.298 48.861,2.298 48.855,2
 SELECT latlon_is_valid(geom) AS should_be_false
 FROM (SELECT st_geomFromText('POLYGON((2.291 148.861,2.298 48.861,2.298 48.855,2.291 48.855,2.291 148.861))', 4326) as geom) sq;
 
-SELECT 'Validity - valid point.' AS TEST;
+SELECT 'latlon_is_valid - valid point.' AS TEST;
 SELECT latlon_is_valid(geom) AS should_be_false
 FROM (SELECT st_geomFromText('POINT(2.294609 48.85835)', 4326) as geom) sq;
 
-SELECT 'Validity - invalid points.' AS TEST;
+SELECT 'latlon_is_valid - invalid points.' AS TEST;
 SELECT latlon_is_valid(geom) AS should_be_false
 FROM (SELECT st_geomFromText('POINT(-181 5)', 4326) as geom) sq;
 
